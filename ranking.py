ee8d4b0bb6ed9418249745953a2f844adbcd2ed5
zander363 <b04505021@ntu.edu.tw>
2018-06-10 03:20:42 +0800

    revise the ranking interface

--- ranking.py
+++ ranking.py
@@ -10,7 +10,6 @@
 import requests 
 
 import simplejson as json
-import pandas as pd
 from simplejson.compat import StringIO
 """
 get the ranking between NTU on kaggle competition
@@ -19,31 +18,37 @@ rank = requests.get("https://www.kaggle.com/c/8900/leaderboard.json?includeBefor
 
 io = StringIO(rank.text)
 data = json.load(io)
-# data = pd.DataFrame.from_dict(io,orient='index')
+
 before = data["beforeUser"]
 near = data["nearUser"]
 after = data["afterUser"]
 
 counter = 1
-print(format("rank","^6"),format('score',"^15"),format('teamName',"<30"))
+print("   ",format("rank","^6"),format('score',"^15"),format('teamName',"<30"))
 for i in before:
+    our = ' '
     if i['teamName'][:4] == "NTU_":
         if i['teamName'] == "NTU_r06942018___":
+            our = '*'
             position = counter
-        print(format(counter,"^6"),format(i['score'],"^15"),format(i['teamName'],"<30"))
+        print('['+our+']',format(counter,"^6"),format(i['score'],"^15"),format(i['teamName'],"<30"))
         counter+=1
 for i in near:
+    our = ' '
     if i['teamName'][:4] == "NTU_":
         if i['teamName'] == "NTU_r06942018___":
+            our = '*'
             position = counter
-        print(format(counter,"^6"),format(i['score'],"^15"),format(i['teamName'],"<30"))
+        print('['+our+']',format(counter,"^6"),format(i['score'],"^15"),format(i['teamName'],"<30"))
         counter+=1
 
 for i in after:
+    our = ' '
     if i['teamName'][:4] == "NTU_":
         if i['teamName'] == "NTU_r06942018___":
+            our = '*'
             position = counter
-        print(format(counter,"^6"),format(i['score'],"^15"),format(i['teamName'],"<30"))
+        print('['+our+']',format(counter,"^6"),format(i['score'],"^15"),format(i['teamName'],"<30"))
         counter+=1
 
 if position < 10:
@@ -54,9 +59,10 @@ if position < 6:
     score = 7.5 
 if position < 4:
     score = 10
+
 print("")
-print("Now we are the ",position,"/ 10  in NTU") 
-print("the ranking point should be",score)
+print("Rank (in NTU) : ",position,"/ 10  ") 
+print("Ranking Point : ",score)
 print("")
 
 
